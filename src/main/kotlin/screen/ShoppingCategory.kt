dce628e6b77c8404add50a4c319e436f2e9883be
jaeYoun <rlawodusx@naver.com>
2022-12-21 22:58:38 +0900

    실습 3-2. Home 화면과 Category 화면으로 분리

---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,37 @@
+package screen
+
+class ShoppingCategory {
+
+    fun showCategories() {
+        val categories = arrayOf("패션", "전자기기", "반려동물용품")
+        for (category in categories) {
+            println("$category")
+        }
+        println("=> 장바구니로 이동하시려면 #을 입력해주세요")
+        var selectedCategory = readLine()
+        // 사용자가 우리가 원하는 값을 입력할때까지 보여주어야 한다.
+        while (selectedCategory.isNullOrBlank()) {
+            println("값을 입력해주세요")
+            selectedCategory = readLine()
+        }
+        if (selectedCategory == "#") {
+            // TODO 1. 장바구니 이동
+        } else {
+
+            /**
+             * categories(목록)에서 contain 함수로 사용자가 입력한 상품이 포함되었는지 확인
+             */
+            if (categories.contains(selectedCategory)) {
+                // TODO 2. 카테고리 상품 목록 보여주기
+            } else {
+                showErrorMessage(selectedCategory)
+            }
+        }
+    }
+
+    private fun showErrorMessage(selectedCategory: String?) {
+        // 사용자가 입력한 값을 알려주고 그 값이 목록에 포함되어 있지 않아 다시 입력을 해야함을 알려준다.
+        println("[$selectedCategory] : 존재하지 않는 카테고리 입니다. 다시 입력해주세요")
+        showCategories()
+    }
+}
--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -4,29 +4,9 @@ class ShoppingHome {
     fun start () {
         showWelcomeMessage()
         showCategories()
-
-
     }
 
-    private fun showCategories() {
-        val categories = arrayOf("패션", "전자기기", "반려동물용품")
-        for (category in categories) {
-            println("$category")
-        }
-        println("=> 장바구니로 이동하시려면 #을 입력해주세요")
-        var selectedCategory = readLine()
-        // 사용자가 우리가 원하는 값을 입력할때까지 보여주어야 한다.
-        while (selectedCategory.isNullOrBlank()) {
-            println("값을 입력해주세요")
-            selectedCategory = readLine()
-        }
-        if (selectedCategory == "#") {
-            // TODO 1. 장바구니 이동
-        } else {
-            // TODO 2. 카테고리 상품 목록 보여주기
-            // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
-        }
-    }
+
 
     private fun showWelcomeMessage() {
         println("안녕하세요, Shoppi에 오신 것을 환영합니다!")
@@ -39,5 +19,10 @@ class ShoppingHome {
             ***=======================================***
         """.trimIndent()
         )
+    }
+
+    private fun showCategories() {
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.showCategories()
     }
 }
